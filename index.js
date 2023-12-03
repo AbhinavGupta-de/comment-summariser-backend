1f37ada200de76022e337f520942634d7e6d429e
Abhinav Gupta <Abhinav.gupta@outlook.de>
2023-12-03 14:11:04 +0530

    forgot to import cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const summarizeRoute = require('./routes/summarize.route');
 const dotenv = require('dotenv');
+const cors = require('cors');
 
 dotenv.config();
 
